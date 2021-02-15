cbdd141be002e2f627f08d746e609322eab73888
perrine1611 <p.toubeau@gmail.com>
2021-02-15 15:28:12 +0100

    test

---
+++ app/models/event.rb
@@ -0,0 +1,24 @@
+class Event < ApplicationRecord
+
+	belongs_to: user
+
+	has_many: attendances
+	has_many: users, through: :attendances
+
+	validates :start_date, presence: true, validate :start_in_future
+	validates :duration, presence: true, validate :multiple_of_5
+	validates :title, presence: true, length: { :within => 5..140}
+	validates :description, presence: true, length: { :within => 20..1000}
+	validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000}
+	validates :location, presence: true
+end
+
+	def start_in_future
+		if start_date != nil && Time.now > start_date
+		puts "La date choisie doit être dans le futur ! "
+	end
+
+	def multiple_of_5
+		if duration != nil && duration % 5 != 0
+		puts "La durée doit être un multiple de 5"
+	end
---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name:"Perrine", email:"perrine@yopmail.com")
+User.create(first_name:"Clara", email:"clara@yopmail.com")
+User.create(first_name:"Claude", email:"claude@yopmail.com")
+User.create(first_name:"Bertrand", email:"bertrand@yopmail.com")
+User.create(first_name:"Martine", email:"martine@yopmail.com")
