6942486728bb26c6019ed929d4247e7fa79e3544
perrine1611 <p.toubeau@gmail.com>
2021-02-16 18:31:05 +0100

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,4 @@ class UsersController < ApplicationController
 		@user = User.find(current_user.id)
 	end
 
-	private
-
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :description)
-    end
-
 end
